002f018dfc7fad42b3263d3edff69c1315b58cef
Emilio P Egido <eaperez@me.com>
2025-11-09 20:34:25 +0100

    Add AppDelegate to handle app termination
    - Exit the app when closing the window from the red button.

--- AgendaT/AgendaTApp.swift
+++ AgendaT/AgendaTApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct AgendaTApp: App {
+	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
