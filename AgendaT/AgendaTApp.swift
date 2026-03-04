aea944f77e11882408ecb2a1d133b1338f3d53f8
Emilio P Egido <eaperez@me.com>
2026-03-04 22:03:09 +0100

    Add Sparkle auto-update support
    - Integrate Sparkle for app updates
    - Add appcast.xml and Info.plist entries (SUFeedURL, SUPublicEDKey)
    - Bump version to 1.2.5 (25).

--- AgendaT/AgendaTApp.swift
+++ AgendaT/AgendaTApp.swift
@@ -1,8 +1,10 @@
 import SwiftUI
+import Sparkle
 
 @main
 struct AgendaTApp: App {
 	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @StateObject private var updaterController = UpdaterController()
 
     var body: some Scene {
 			//Single instance app
@@ -17,6 +19,24 @@ struct AgendaTApp: App {
 			// Window resizability derived from the window’s content
 			// macOS 13 Ventura or newer
 		.windowResizability(.contentSize)
+        
+        .commands {
+              // Updater menu
+              CommandGroup(after: .appInfo) {
+                  Button(
+                      NSLocalizedString(
+                          "Check for Updates...",
+                          comment: "Menu item to check for app updates"
+                      ),
+  //                    systemImage: "square.and.arrow.down.badge.checkmark"
+                      systemImage: "arrow.triangle.2.circlepath"
+                  ) {
+                      updaterController.checkForUpdates()
+                  }
+                  .keyboardShortcut("u", modifiers: [.command])
+                  .disabled(!updaterController.canCheckForUpdates)
+              }
+          }
 
     }
 }
